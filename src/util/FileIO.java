bd08c0615f1fc37e52bea0133e98f451c2217ae7
rs280 <rishabhsahoo1@gmail.com>
2022-02-15 21:41:54 -0800

    adding in stuff

--- src/util/FileIO.java
+++ src/util/FileIO.java
@@ -1,17 +1,29 @@
-
 package util;
 
 import java.io.*;
+import java.util.Properties;
+
 import model.Automobile;
 import exception.AutoException;
 
 public class FileIO {
-	/* File pattern: newline (\n) separates different optionSet colon ":" separates
-	 * optionSetName and optionSetOptions optionSetOptions may span multiple lines
+
+	public Automobile fileToAutomobile(String fileName) throws AutoException {
+		Automobile automobileObject = new model.Automobile();
+		addToAutomobile(fileName, automobileObject);
+		return automobileObject;
+	}
+
+	/** File pattern: newline (\n) separates different optionSet colon ":"
+	 * separates
+	 * optionSetName and optionSetOptions optionSetOptions may span multiple
+	 * lines
 	 * as long as no new optionSetName is found comma "," separates different
-	 * optionSetOptions slash "/" separates different option values */
-	public boolean read(String fileName, Automobile autoObj) throws AutoException {
-		boolean returnValue = false;
+	 * optionSetOptions slash "/" separates different option values
+	 * @param fileName The file name
+	 * @param automobileObject The Automobile object
+	 * @throws AutoException */
+	public void addToAutomobile(String fileName, Automobile automobileObject) throws AutoException {
 		String optionSetOptions, optionSetName, lineNext;
 
 		BufferedReader reader = null;
@@ -23,31 +35,34 @@ public class FileIO {
 			while ((lineNext = reader.readLine()) != null) {
 				// optionSet
 				if (lineNext.indexOf(':') != -1) {
+					/* OptionSet name was found */
 					String[] optionSetParts = lineNext.split(":");
 					optionSetName = optionSetParts[0].trim();
 					optionSetOptions = optionSetParts[1].trim();
 					try {
-						optionSetObjectIndex = autoObj.addOptionSet(optionSetName);
+						optionSetObjectIndex = automobileObject.addOptionSet(optionSetName);
 					} catch (AutoException e) {
 						optionSetObjectIndex = -1;
 					}
 				} else {
-					/* whole line is options if the optionSetName not found This allows options to
-					 * be split on multiple lines for file readability. */
+					/* optionSet name not found, so the whole line must be
+					 * options.
+					 * We approach line parsing this way to allows options to be
+					 * split on multiple
+					 * lines for file readability. */
 					optionSetOptions = lineNext;
 				}
 				if (optionSetObjectIndex != -1) {
 					try {
-						optionSetOptionsProcess(autoObj, optionSetObjectIndex, optionSetOptions);
+						optionSetOptionsProcess(automobileObject, optionSetObjectIndex, optionSetOptions);
 					} catch (AutoException e) {
 						optionSetObjectIndex = -1;
 					}
 				} else if (optionSetName != null && !optionSetName.equals("") && optionSetOptions != null
 					&& !optionSetOptions.equals("")) {
-					autoObj.setOptionSetOptionNameReserved(optionSetName, optionSetOptions);
+					automobileObject.setOptionSetOptionNameReserved(optionSetName, optionSetOptions);
 				}
 			}
-			returnValue = true;
 		} catch (FileNotFoundException e) {
 			throw new exception.AutoException(200);
 		} catch (IOException e) {
@@ -58,13 +73,17 @@ public class FileIO {
 					reader.close();
 				}
 			} catch (IOException e) {
-				// nothing
+				throw new exception.AutoException(200);
 			}
 		}
-		return returnValue;
 	}
 
-	private boolean optionSetOptionsProcess(Automobile autoObj, int optionSetObjectIndex, String optionSetOptions)
+	/** Parse an option string into the Automobile OptionSet index
+	 * @param automobileObject The automobile object
+	 * @param optionSetObjectIndex The OptionSet index
+	 * @param optionSetOptions The option string
+	 * @throws AutoException */
+	private void optionSetOptionsProcess(Automobile automobileObject, int optionSetObjectIndex, String optionSetOptions)
 		throws AutoException {
 		if (optionSetOptions.equals("")) {
 			throw new exception.AutoException(101);
@@ -90,46 +109,96 @@ public class FileIO {
 					if (optionPrice.equals("")) {
 						new exception.AutoException(103, true); // warning
 					}
-					autoObj.addOptionSetOption(optionSetObjectIndex, optionName, Double.parseDouble(optionPrice));
+					automobileObject.addOptionSetOption(optionSetObjectIndex, optionName,
+						Double.parseDouble(optionPrice));
 				} else {
-					autoObj.addOptionSetOption(optionSetObjectIndex, optionPart.trim(), 0);
+					automobileObject.addOptionSetOption(optionSetObjectIndex, optionPart.trim(), 0);
 				}
 			} else {
 				new exception.AutoException(102, true); // warning
 			}
 		}
-		return true;
 	}
 
-	public boolean serialize(String fileName, Automobile autoObj) throws AutoException {
-		boolean returnValue = false;
+	/** serializes and Automobile object object as a file
+	 * @param fileName The File name
+	 * @param automobileObject The Automobile object
+	 * @throws AutoException */
+	public void serialize(String fileName, Automobile automobileObject) throws AutoException {
 		try {
 			FileOutputStream fileOut = new FileOutputStream(fileName);
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(autoObj);
+			out.writeObject(automobileObject);
 			out.close();
 			fileOut.close();
-			returnValue = true;
+		} catch (NullPointerException e) {
+			throw new exception.AutoException(300);
 		} catch (IOException e) {
 			throw new exception.AutoException(300);
 		}
-		return returnValue;
 	}
 
-	public Automobile deserialize(String fileName) {
-		Automobile autoObj = null;
+	/** deserializes a file as an Automobile object
+	 * @param fileName The File name
+	 * @return The Automobile object
+	 * @throws AutoException */
+	public Automobile deserialize(String fileName) throws AutoException {
+		Automobile automobileObject = null;
 		try {
 			FileInputStream fileIn = new FileInputStream(fileName);
 			ObjectInputStream in = new ObjectInputStream(fileIn);
-			autoObj = (Automobile) in.readObject();
+			automobileObject = (Automobile) in.readObject();
 			in.close();
 			fileIn.close();
+		} catch (NullPointerException e) {
+			throw new exception.AutoException(300);
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new exception.AutoException(300);
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+			throw new exception.AutoException(300);
 		}
-		return autoObj;
+		return automobileObject;
+	}
+
+	/** serializes and Automobile object object as a file
+	 * @param socketStreamOut The output stream
+	 * @param automobileProperties The Automobile properties
+	 * @throws AutoException */
+	public void serializeToStream(OutputStream socketStreamOut, Automobile automobileObject) throws AutoException {
+		if (automobileObject == null) {
+			throw new exception.AutoException(300);
+		}
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(socketStreamOut);
+			out.writeObject(automobileObject);
+			// do not close stream. let caller do this.
+			// out.close();
+		} catch (NullPointerException e) {
+			throw new exception.AutoException(300);
+		} catch (IOException e) {
+			throw new exception.AutoException(300);
+		}
+	}
+
+	/** deserializes a file as an Automobile object
+	 * @param socketStreamIn The input stream
+	 * @return The Automobile properties
+	 * @throws AutoException */
+	public Automobile deserializeFromStream(InputStream socketStreamIn) throws AutoException {
+		Automobile automobileObject = null;
+		try {
+			ObjectInputStream in = new ObjectInputStream(socketStreamIn);
+			automobileObject = (Automobile) in.readObject();
+			// do not close stream. let caller do this.
+			// in.close();
+		} catch (NullPointerException e) {
+			throw new exception.AutoException(300);
+		} catch (IOException e) {
+			throw new exception.AutoException(300);
+		} catch (ClassNotFoundException e) {
+			throw new exception.AutoException(300);
+		}
+		return automobileObject;
 	}
 
 	/* print() and toString() */
