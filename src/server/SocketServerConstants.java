62cd18f5969269fe983883a3508ed62b9bb2175b
rs280 <rishabhsahoo1@gmail.com>
2022-02-17 17:14:30 -0800

    adding in test files

--- src/server/SocketServerConstants.java
+++ src/server/SocketServerConstants.java
@@ -3,7 +3,7 @@ package server;
 public interface SocketServerConstants
 {
     int iECHO_PORT = 7;
-    int iDAYTIME_PORT = 3000; //13
+    int iDAYTIME_PORT = 4444; //13
     int iSMTP_PORT = 25;
     boolean DEBUG = true;
 }
